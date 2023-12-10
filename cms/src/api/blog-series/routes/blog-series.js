b5f862d370c816ac99ef162890b13400a5146b1c
Matt Richards <mattc0m@gmail.com>
2023-12-10 02:25:34 +0000

    delete everything

--- cms/src/api/blog-series/routes/blog-series.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * blog-series router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::blog-series.blog-series');
